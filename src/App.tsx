f47c43735119dbf1d6d9fa18bad787b984321077
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-08-24 02:52:57 +0300

    feat: rename project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 const App = () => {
   return (
     <main>
       <h1>Miralab Management System</h1>
+      <Button>Click me</Button>
     </main>
   );
 };
