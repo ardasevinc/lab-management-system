9db4034d19b19144f0008205b67547016e01f547
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-08-31 13:37:46 +0300

    feat: add error page and root layout init

---
+++ src/routes/Error.tsx
@@ -0,0 +1,72 @@
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+
+const RouteError = (error) => {
+  return (
+    <>
+      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center'>
+        Oopsie Woopsie!
+      </h1>
+      <div className='flex flex-col justify-center items-center'>
+        <img
+          src='/images/error.png'
+          alt='sad robot'
+          className='rounded-lg w-full max-w-md'
+        />
+        <h2 className='mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>
+          Route Error {error.status && `(${error.status})`}
+        </h2>
+        {error.statusText && (
+          <blockquote className='mt-6 border-l-2 pl-6 italic'>
+            {error.statusText}
+          </blockquote>
+        )}
+        <p className='leading-7 [&:not(:first-child)]:mt-6'>
+          We will whip the developers extra hard to make sure this doesn't
+          happen again!
+        </p>
+      </div>
+    </>
+  );
+};
+
+const OtherError = (error) => {
+  return (
+    <>
+      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+        What was that?
+      </h1>
+      <div className='flex'>
+        <img src='/images/error.png' alt='sad robot' className='w-1/2' />
+        <div>
+          <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'>
+            Route Error {error.status && `(${error.status})`}
+          </h2>
+          {error.statusText && (
+            <blockquote className='mt-6 border-l-2 pl-6 italic'>
+              {error.statusText}
+            </blockquote>
+          )}
+          <p className='leading-7 [&:not(:first-child)]:mt-6'>
+            We will whip the developers extra hard to make sure this doesn't
+            happen again!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Error = () => {
+  const error = useRouteError();
+  return (
+    <main className='container flex flex-col justify-center items-center h-screen gap-y-16'>
+      {isRouteErrorResponse(error) ? (
+        <RouteError error={error} />
+      ) : (
+        <OtherError error={error} />
+      )}
+    </main>
+  );
+};
+
+export default Error;
---
+++ src/routes/Root.tsx
@@ -0,0 +1,11 @@
+const Root = () => {
+  return (
+    <div className='min-h-screen'>
+      <main className='container'>
+        <h1>Root</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Root;
